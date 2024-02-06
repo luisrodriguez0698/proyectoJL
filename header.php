a61cb547bdb14866791182b22fa611aa2721bb24
luisrodriguez0698 <eduardo.rodriguez@cpavs.mx>
2024-02-06 10:01:54 -0600

    primero ajustes de codigo

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<header>
+    <nav class="nav size">
+        <div class="logo">
+            <h3>name</h3>
+        </div>
+        <ul class="menu">
+            <li>inicio</li>
+            <li>servicios</li>
+            <li>preios</li>
+            <li>testimoios</li>
+            <li>contacto</li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include_once "./header.php";
+?>
+
+<body>
+    <main>
+        <section class="intro">
+            <div class="intro_grid size">
+                <div class="intro_desc">
+                    <h1>Realiza tu sitio web voz <br> fiero tu modoo...</h1>
+                    <button>var más</button>
+                </div>
+                <div class="intro_">
+
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+
+<?php
+include_once "./footer.php";
+?>
