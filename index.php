7488be6762997a4e34ec9f6123f3ac51a157915c
luisrodriguez0698 <eduardo.rodriguez@cpavs.mx>
2024-02-06 19:04:49 -0600

    mejorar de cursor e imagenes

--- index.php
+++ index.php
@@ -1,42 +1,65 @@
 <?php
 include_once "./header.php";
-include_once "./menu.php";
 ?>
-<div id="custom-cursor"></div>
-<div id="cursor_back"></div>
-<main>
-    <section class="intro">
-        <div class="intro_grid size">
-            <div class="intro_desc">
-                <h1>Realiza tu sitio web voz <br> fiero tu modoo...</h1>
-                <button class="intro_btn">ver más</button>
-            </div>
-            <div class="intro_carousel">
-                <div id="introCarouselControls" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="img/carousel/C_1.svg" class="d-block mx-auto img-fluid" alt="C_1">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="img/carousel/C_2.svg" class="d-block mx-auto img-fluid" alt="C_2">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="img/carousel/C_3.svg" class="d-block mx-auto img-fluid" alt="C_3">
+
+<div id="loader-wrapper">
+    <span class="loader"></span>
+</div>
+
+<div id="content">
+    <?php
+    include_once "./menu.php";
+    ?>
+    <main>
+        <section class="intro">
+            <div class="intro_grid size">
+                <div class="intro_desc">
+                    <h1>Realiza tu sitio web voz <br> fiero tu modoo...</h1>
+                    <button class="intro_btn">ver más</button>
+                </div>
+                <div class="intro_carousel">
+                    <div id="introCarouselControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="img/carousel/C_1.png" class="d-block mx-auto img-fluid" alt="C_1">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img/carousel/C_2.png" class="d-block mx-auto img-fluid" alt="C_2">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="img/carousel/C_3.png" class="d-block mx-auto img-fluid" alt="C_3">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="intro_wave">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#333C4E" fill-opacity="1" d="M0,128L20,144C40,160,80,192,120,181.3C160,171,200,117,240,128C280,139,320,213,360,234.7C400,256,440,224,480,208C520,192,560,192,600,208C640,224,680,256,720,261.3C760,267,800,245,840,234.7C880,224,920,224,960,197.3C1000,171,1040,117,1080,128C1120,139,1160,213,1200,234.7C1240,256,1280,224,1320,197.3C1360,171,1400,149,1420,138.7L1440,128L1440,320L1420,320C1400,320,1360,320,1320,320C1280,320,1240,320,1200,320C1160,320,1120,320,1080,320C1040,320,1000,320,960,320C920,320,880,320,840,320C800,320,760,320,720,320C680,320,640,320,600,320C560,320,520,320,480,320C440,320,400,320,360,320C320,320,280,320,240,320C200,320,160,320,120,320C80,320,40,320,20,320L0,320Z"></path>
-            </svg>
-        </div>
-    </section>
-</main>
+            <div class="intro_wave">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <path fill="#333C4E" fill-opacity="1" d="M0,128L20,144C40,160,80,192,120,181.3C160,171,200,117,240,128C280,139,320,213,360,234.7C400,256,440,224,480,208C520,192,560,192,600,208C640,224,680,256,720,261.3C760,267,800,245,840,234.7C880,224,920,224,960,197.3C1000,171,1040,117,1080,128C1120,139,1160,213,1200,234.7C1240,256,1280,224,1320,197.3C1360,171,1400,149,1420,138.7L1440,128L1440,320L1420,320C1400,320,1360,320,1320,320C1280,320,1240,320,1200,320C1160,320,1120,320,1080,320C1040,320,1000,320,960,320C920,320,880,320,840,320C800,320,760,320,720,320C680,320,640,320,600,320C560,320,520,320,480,320C440,320,400,320,360,320C320,320,280,320,240,320C200,320,160,320,120,320C80,320,40,320,20,320L0,320Z"></path>
+                </svg>
+            </div>
+        </section>
 
+        <section class="services">
+            <div class="services_grid">
+                <div class="services_target">
+                    <div class="st_desc">
+                        <h3>Desarrollo de sitios web</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut perferendis amet voluptas rem possimus eum cupiditate officia ipsum adipisci, quaerat minus dignissimos unde tempora porro harum accusantium autem nisi.</p>
+                    </div>
+                    <div>
+                        <img src="img/carousel/C_1.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</div>
 
-
+<div id="cursor">
+    <div class="cursor-dot-outline"></div>
+    <div class="cursor-dot"></div>
+</div>
 
 <?php
 include_once "./footer.php";
--- js/app.js
+++ js/app.js
@@ -1,29 +1,165 @@
 document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener("scroll", function() {
-        var header = document.getElementById("menuNav");
 
-        // Obtener la posición actual de la barra de desplazamiento
-        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
-        // Cambiar la clase del header dependiendo de la posición del scroll
-        if (scrollPosition > 50) {
-            header.classList.add("sticky");
-        } else {
-            header.classList.remove("sticky");
+    //menu stick
+    $(document).ready(function(){
+        var altura = $('#menuNav').offset().top;
+        
+        $(window).on('scroll', function(){
+            if ( $(window).scrollTop() > altura ){
+                $('#menuNav').addClass('menu-fixed');
+            } else {
+                $('#menuNav').removeClass('menu-fixed');
+            }
+        });
+    
+    });
+
+
+    //loader
+    setTimeout(function() {
+        // Oculta el loader y muestra el contenido después de cierto tiempo
+        document.getElementById("loader-wrapper").style.display = "none";
+        document.getElementById("cursor").style.display = "block";
+        document.getElementById("content").style.display = "block";
+
+    }, 3000); // Simula un tiempo de carga de 2 segundos
+
+
+    var cursor = {
+        delay: 8,
+        _x: 0,
+        _y: 0,
+        endX: window.innerWidth / 2,
+        endY: window.innerHeight / 2,
+        cursorVisible: true,
+        cursorEnlarged: false,
+        CoursorColor: true,
+        $dot: document.querySelector(".cursor-dot"),
+        $outline: document.querySelector(".cursor-dot-outline"),
+      
+        init: function () {
+          // Configuracion de tamaños de elementos
+          this.setupEventListeners();
+          this.animateDotOutline();
+        },
+      
+        setupEventListeners: function () {
+          var self = this;
+      
+          // Ancho del hovering
+          document.querySelectorAll("a").forEach(function (el) {
+            el.addEventListener("mouseover", function () {
+              self.CoursorColor = false;
+              self.toggleColor();
+              self.cursorEnlarged = true;
+              self.toggleCursorSize();
+            });
+            el.addEventListener("mouseout", function () {
+              self.CoursorColor = true;
+              self.toggleColor();
+              self.cursorEnlarged = false;
+              self.toggleCursorSize();
+            });
+          });
+
+          document.querySelectorAll("button").forEach(function (el) {
+            el.addEventListener("mouseover", function () {
+              self.CoursorColor = false;
+              self.toggleColor();
+              self.cursorEnlarged = true;
+              self.toggleCursorSize();
+            });
+            el.addEventListener("mouseout", function () {
+              self.CoursorColor = true;
+              self.toggleColor();
+              self.cursorEnlarged = false;
+              self.toggleCursorSize();
+            });
+          });
+      
+          // Eventos click
+          document.addEventListener("mousedown", function () {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+          });
+          document.addEventListener("mouseup", function () {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+          });
+      
+          document.addEventListener("mousemove", function (e) {
+            // Mostar el cursor
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+      
+            // Posición de el punto
+            self.endX = e.pageX;
+            self.endY = e.pageY;
+            self.$dot.style.top = self.endY + "px";
+            self.$dot.style.left = self.endX + "px";
+          });
+      
+          // Oculta/mostrar cursor
+          document.addEventListener("mouseenter", function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+          });
+      
+          document.addEventListener("mouseleave", function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+          });
+        },
+      
+        animateDotOutline: function () {
+          var self = this;
+      
+          self._x += (self.endX - self._x) / self.delay;
+          self._y += (self.endY - self._y) / self.delay;
+          self.$outline.style.top = self._y + "px";
+          self.$outline.style.left = self._x + "px";
+          requestAnimationFrame(this.animateDotOutline.bind(self));
+        },
+      
+        toggleCursorSize: function () {
+          var self = this;
+      
+          if (self.cursorEnlarged) {
+            self.$dot.style.transform = "translate(-50%, -50%) scale(0.75)";
+            self.$outline.style.transform = "translate(-50%, -50%) scale(1.5)";
+          } else {
+            self.$dot.style.transform = "translate(-50%, -50%) scale(1)";
+            self.$outline.style.transform = "translate(-50%, -50%) scale(1)";
+          }
+        },
+      
+        toggleColor: function () {
+          if (this.CoursorColor) {
+            this.$dot.style.visibility = "visible";
+            this.$outline.style.visibility = "visible";
+          } else {
+            this.$dot.style.visibility = "hidden";
+            this.$outline.style.visibility = "hidden";
+          }
+        },
+      
+        toggleCursorVisibility: function () {
+          var self = this;
+          if (self.cursorVisible) {
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+          } else {
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+          }
         }
-    });
-
-
-
-    var customCursor = document.getElementById("custom-cursor");
-    var cursor_back = document.getElementById("cursor_back");
-
-    document.addEventListener("mousemove", function(event) {
-        var x = event.clientX - 10;
-        var y = event.clientY - 10;
-
-        customCursor.style.transform = "translate(" + x + "px, " + y + "px)";
-        cursor_back.setAttribute("style", "top: " + (event.pageY -10) + "px; left: " + (event.pageX - 15) + "px;");
-    });
+      };
+      
+      cursor.init();
 
 
 });
